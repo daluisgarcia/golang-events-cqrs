b46fec6f31907e74347398a00fabca30a6698b2d
Daniel Luis <daluisgarcia@gmail.com>
2022-12-06 13:58:37 -0400

    Events methods params fixed

--- events/event.go
+++ events/event.go
@@ -9,8 +9,8 @@ import (
 type EventStore interface {
 	Close()
 	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
-	SuscribeCreatedFeed(ctx context.Context) (<-chan *models.Feed, error)
-	OnCreateFeed(f func(CreatedFeedMessage)) error // Callback that reacts when a new feed has been created
+	SuscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
+	OnCreateFeed(ctx context.Context, f func(CreatedFeedMessage)) error // Callback that reacts when a new feed has been created
 }
 
 var eventStore EventStore
@@ -27,10 +27,10 @@ func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
 	return eventStore.PublishCreatedFeed(ctx, feed)
 }
 
-func SuscribeCreatedFeed(ctx context.Context) (<-chan *models.Feed, error) {
+func SuscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
 	return eventStore.SuscribeCreatedFeed(ctx)
 }
 
-func OnCreateFeed(f func(CreatedFeedMessage)) error {
-	return eventStore.OnCreateFeed(f)
+func OnCreateFeed(ctx context.Context, f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreateFeed(ctx, f)
 }
