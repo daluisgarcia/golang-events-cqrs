ff1edef1338fca9759262b19e1fcbfc7d9d54ad4
Daniel Luis <daluisgarcia@gmail.com>
2022-12-06 10:41:16 -0400

    Events messages and store interfaces added

---
+++ events/event.go
@@ -0,0 +1,36 @@
+package events
+
+import (
+	"context"
+
+	"github.com/daluisgarcia/golang-events-cqrs/models"
+)
+
+type EventStore interface {
+	Close()
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
+	SuscribeCreatedFeed(ctx context.Context) (<-chan *models.Feed, error)
+	OnCreatedFeed(f func(CreatedFeedMessage)) error // Callback that reacts when a new feed has been created
+}
+
+var eventStore EventStore
+
+func SetEventStore(eStore EventStore) {
+	eventStore = eStore
+}
+
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+
+func SuscribeCreatedFeed(ctx context.Context) (<-chan *models.Feed, error) {
+	return eventStore.SuscribeCreatedFeed(ctx)
+}
+
+func OnCreatedFeed(f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreatedFeed(f)
+}
